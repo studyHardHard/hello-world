94cd008ebbfe2611691f9cc659bbf01581b1942f
admin <admin@admin-PC>
2018-06-10 16:18:12 +0800

    fghjkl

--- src/com/text/GUI.java
+++ src/com/text/GUI.java
@@ -6,17 +6,15 @@ import java.awt.Frame;
 
 public class GUI {
 	public static void main(String[] args) {  
-		         /* Frame f = new Frame("my awt");  
+		         Frame f = new Frame("my awt");  
 		          f.setSize(500,400);  
 		          f.setLocation(300,200);  
 		          f.setLayout(new FlowLayout());  
-		          Button b=new Button("我是一个按钮");   
-		          b.
-		          f.add(b);  		           
+		          Button b=new Button("我是一个按钮");        
 		         // f.addWindowListener(new MyWin());  
 		            
 		         f.setVisible(true);  
-		         System.out.println("Hello world!");  */
+		         System.out.println("Hello world!");
 				String status = "03"; 
 		        if (status == "03") {
 					System.out.println("****");
